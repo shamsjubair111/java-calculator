c699a5daebee72929ca91805ff3772909719eb2c
shamsjubair111 <shamsjubair.sj@gmail.com>
2024-06-23 22:32:00 +0600

    solved fibonacci number by myself

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,81 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
 
+//        Print number form 1-5
+
+//        for(int i =1 ; i <=5; i++){
+//            System.out.println(i);
+//        }
+
+//        print numbers from 1 to n (where will input value of n)
+
+//        System.out.println("Enter Value of N: ");
+//
+//        Scanner number  = new Scanner(System.in);
+//        int inputNumber = number.nextInt();
+//
+//        for(int i = 0; i<=inputNumber ;i++){
+//
+//            System.out.println(i);
+//
+//        }
+
+
+
+//        find the maximun of three given numbers
+
+
+//        int a = 10;
+//        int b = 620;
+//        int c = 30;
+//
+//        if(a >b && a> c){
+//            System.out.println("Max is a with value: " + a);
+//        }
+//        else if(b >a && b> c){
+//            System.out.println("Max is b with value: " + b);
+//        }
+//        if(c >a && c> b){
+//            System.out.println("Max is c with value: " + c);
+//        }
+
+
+
+//        fibonacci numbers
+
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int nextNumber = 0;
+
+        System.out.println("Enter The index of number you want to find: ");
+
+        Scanner sc = new Scanner(System.in);
+        int inputNumber = sc.nextInt();
+
+        System.out.println("You entered: " + inputNumber);
+
+        for(int i = 0; i<=inputNumber-2; i++){
+            nextNumber  = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
+
+        System.out.println("The final answer is: " + nextNumber);
+
+
+
+
+
         
+
+
+
 
     }
 }
