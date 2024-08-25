5cb32528205fc31930424306d088c241aae39ca5
shamsjubair111 <shamsjubair.sj@gmail.com>
2024-08-25 11:16:33 +0600

    tried once again and solved the calculator problem by myself this time.

--- src/Main.java
+++ src/Main.java
@@ -287,52 +287,120 @@ public class Main {
 // building a calculator program
 
 
-        System.out.println("Enter + - * / % as operators or x or X to terminate the program");
+//        System.out.println("Enter + - * / % as operators or x or X to terminate the program");
+//        Scanner scanner = new Scanner(System.in);
+//
+//       int result = 0;
+//
+//        while(true){
+//            char operator = scanner.next().charAt(0);
+//            if(operator == 'x' || operator == 'X'){
+//                break;
+//            }
+//            else if(operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '%'){
+//                System.out.println("Enter First Number");
+//                int firstNum  = scanner.nextInt();
+//                System.out.println("Enter Second Number");
+//                int secondNum = scanner.nextInt();
+//
+//                if(operator == '+'){
+//                     result = firstNum + secondNum;
+//
+//
+//                }
+//                else if(operator == '-'){
+//                     result = firstNum - secondNum;
+//                }
+//                else if(operator == '*'){
+//                     result = firstNum * secondNum;
+//                }
+//                else if(operator == '/'){
+//                    if(secondNum != 0){
+//                         result = firstNum / secondNum;
+//                    }
+//                    else{
+//                        System.out.println("Can not divide " + firstNum  + " by " + secondNum);
+//                    }
+//                }
+//                else if(operator == '%'){
+//                     result  = firstNum % secondNum;
+//                }
+//
+//                System.out.println("Result is: " + result);
+//            }
+//            else{
+//                System.out.println("Invalid Operator");
+//                break;
+//            }
+//        }
+
+//        int number = 1234567;
+//        String result = String.valueOf(number);
+//        String finalResult = "";
+        int revNum = 0;
+//        System.out.println(number);
+//
+//        for(int i = result.length() -1 ; i>=0;i--){
+//            finalResult = finalResult + result.charAt(i);
+//        }
+//        System.out.println(finalResult);
+
+//             while(number > 0){
+//                 int rem = number % 10;
+//                 finalResult = finalResult + rem;
+//                 number = number / 10;
+//             }
+//        System.out.println(finalResult);
+
+//        while(number > 0){
+//            int rem = number % 10;
+//            revNum = revNum * 10 + rem;
+//            number = number / 10;
+//        }
+//        System.out.println(revNum);
+
+
+//        Calculator problem once again
+
+        System.out.println("Enter The operation First and then two numbers to perform the operation and see the result:");
         Scanner scanner = new Scanner(System.in);
 
-       int result = 0;
-
-        while(true){
-            char operator = scanner.next().charAt(0);
-            if(operator == 'x' || operator == 'X'){
-                break;
-            }
-            else if(operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '%'){
-                System.out.println("Enter First Number");
-                int firstNum  = scanner.nextInt();
-                System.out.println("Enter Second Number");
-                int secondNum = scanner.nextInt();
-
-                if(operator == '+'){
-                     result = firstNum + secondNum;
-
-
-                }
-                else if(operator == '-'){
-                     result = firstNum - secondNum;
-                }
-                else if(operator == '*'){
-                     result = firstNum * secondNum;
-                }
-                else if(operator == '/'){
-                    if(secondNum != 0){
-                         result = firstNum / secondNum;
-                    }
-                    else{
-                        System.out.println("Can not divide " + firstNum  + " by " + secondNum);
-                    }
-                }
-                else if(operator == '%'){
-                     result  = firstNum % secondNum;
-                }
-
-                System.out.println("Result is: " + result);
-            }
-            else{
-                System.out.println("Invalid Operator");
-                break;
-            }
-        }
+      while(true){
+          char operator = scanner.next().charAt(0);
+          if(operator == 'x'){
+              break;
+          }
+          else if(operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '%'){
+              System.out.println("Enter First Number: ");
+              int firstNum = scanner.nextInt();
+              System.out.println("Enter Second Number: ");
+              int secondNum = scanner.nextInt();
+              if(operator == '+'){
+                  System.out.println(firstNum + secondNum);
+              }
+              else if(operator == '-'){
+                  System.out.println(firstNum - secondNum);
+              }
+              else if(operator == '*'){
+                  System.out.println(firstNum * secondNum);
+              }
+              else if(operator == '/'){
+                 if(secondNum != 0){
+                     System.out.println(firstNum / secondNum);
+                 }
+                 else{
+                     System.out.println("Small Number Can't be Divided by Large Number");
+                 }
+              }
+              else if(operator == '%'){
+                  System.out.println(firstNum % secondNum);
+              }
+          }
+          else{
+              System.out.println("Invalid Operation");
+              break;
+          }
+      }
 
 
     }
